24e3120e4ff7c26b4d181c7959028cabb6375615
Matt Styles <matt@veryfizzyjelly.com>
2016-04-19 09:33:37 +0100

    #add basic physics

--- fsm-react/states/main.js
+++ fsm-react/states/main.js
@@ -2,9 +2,13 @@
 import React, { Component } from 'react'
 import Immutable from 'immutable'
 import state from '../appstate'
-import { KeySignal, TickSignal } from 'raid-addons'
+import { curry, compose, KeySignal, TickSignal } from 'raid-addons'
 import { STATES } from '../constants'
 import styles from '../styles'
+
+const DEGREES = Math.PI / 180
+const THRUST = .5
+const DRAG = .95
 
 /**
  * Model
@@ -47,27 +51,59 @@ export default class Main extends Component {
     this.keyRelease = keys.register( src => {
       return src.subscribe( event => {
         if ( event.keys.has( '<left>' ) ) {
-          event.model.cursor( 'rot' ).update( rotation => --rotation )
+          event.model.cursor( 'rot' ).update( rotation => rotation - 3 )
         }
 
         if ( event.keys.has( '<right>' ) ) {
-          event.model.cursor( 'rot' ).update( rotation => ++rotation )
+          event.model.cursor( 'rot' ).update( rotation => rotation + 3 )
         }
 
         if ( event.keys.has( '<up>' ) ) {
-          event.model.cursor( 'thrust' ).update( thrust => 10 )
+          event.model.cursor( 'thrust' ).update( thrust => THRUST )
         }
       })
     })
 
-    // this.tickRelease = tick.register( src => {
-    //   src.subscribe( event => {
-    //     let dt = event.delta * .25
-    //     event.model.update( ship => {
-    //       return ship.update( 'rot', rot => rot + dt )
-    //     })
-    //   })
-    // })
+    this.tickRelease = tick.register( src => {
+      const momentum = curry( ( dt, model ) => {
+        return Object.assign( model, {
+          x: model.x + dt * model.vx,
+          y: model.y + dt * model.vy
+        })
+      })
+
+      const thrust = curry( ( dt, model ) => {
+        if ( !model.thrust ) {
+          return model
+        }
+
+        return Object.assign( model, {
+          vx: model.vx + ( model.thrust * dt * Math.cos( model.rot * DEGREES ) ),
+          vy: model.vy + ( model.thrust * dt * Math.sin( model.rot * DEGREES ) ),
+          thrust: 0
+        })
+      })
+
+      const drag = curry( ( dt, model ) => {
+        let dx = model.vx * DRAG
+        let dy = model.vy * DRAG
+        return Object.assign( model, {
+          vx: Math.abs( dx ) < .15 ? 0 : dx,
+          vy: Math.abs( dy ) < .15 ? 0 : dy
+        })
+      })
+
+      src.subscribe( event => {
+        let dt = event.delta * .05
+        let update = compose(
+          momentum( dt ),
+          thrust( dt ),
+          drag( dt )
+        )
+
+        event.model.merge( update( event.model.toJS() ) )
+      })
+    })
   }
 
   componentWillUnmount() {
@@ -80,11 +116,14 @@ export default class Main extends Component {
     let style = styles.merge( styles.ship, {
       left: ship.get( 'x' ),
       top: ship.get( 'y' ),
-      transform: `rotate( ${ ship.get( 'rot' ) }deg )`
+      transform: `rotate( ${ ship.get( 'rot' ) + 90 }deg )`
     })
 
     return (
       <div style={ styles.fit }>
+        <pre style={{ color: 'white' }}>
+          { JSON.stringify( ship.toJSON(), null, '  ' ) }
+        </pre>
         <img
           src="/fsm-react/assets/ship.png"
           style={ style }
